69d5da70d39b45b43c47ec3192f526f3cb62024d
Edmond-Mawuli-K <edmondmawuli123@gmail.com>
2025-12-15 00:06:54 +0000

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -33,13 +33,12 @@ CREATE TABLE Customers (
 );
 
 -- ORDERS TABLE
-CREATE TABLE Orders (
+CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     CONSTRAINT fk_orders_customer
-        FOREIGN KEY (customer_id)
-        REFERENCES CUSTOMERS(customer_id)
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -51,14 +50,11 @@ CREATE TABLE Order_Details (
     book_id INT,
     quantity DOUBLE NOT NULL,
     CONSTRAINT fk_orderdetails_order
-        FOREIGN KEY (order_id)
-        REFERENCES ORDERS(order_id)
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     CONSTRAINT fk_orderdetails_book
-        FOREIGN KEY (book_id)
-        REFERENCES BOOKS(book_id)
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
-
